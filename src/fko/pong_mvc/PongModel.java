cff89ff07582e5499e53112ca5c9d7e6e3111d11
Frank Kopp <frank@familie-kopp.de>
2018-01-01 14:38:42 +0100

    Renamed package from pong to pong_mvc

--- src/fko/pong_mvc/PongModel.java
+++ src/fko/pong_mvc/PongModel.java
@@ -21,9 +21,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-package fko.pong;
+package fko.pong_mvc;
 
-import fko.pong.PongSounds.Clips;
+import fko.pong_mvc.PongSounds.Clips;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.property.BooleanProperty;
--- src/fko/pong_mvc/PongPane.java
+++ src/fko/pong_mvc/PongPane.java
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-package fko.pong;
+package fko.pong_mvc;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
